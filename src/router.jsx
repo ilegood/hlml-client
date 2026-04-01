9f296add0ba33145dc8ab2368cb0f886140925e4
PeanutPizza <74494029+ilegood@users.noreply.github.com>
2026-04-01 22:18:07 +0900

    styles.css add

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import RegisterPage from "./Pages/RegisterPage";
 import Layout from "./Components/Layout";
+import MainPage from "./Pages/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <App /> },
+      { index: true, element: <MainPage /> },
       { path: "/register", element: <RegisterPage /> },
     ],
   },
