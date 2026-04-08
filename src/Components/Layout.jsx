f851c6f24d11dbefcec8d46fb5b8bb2105dfc0bc
PeanutPizza <74494029+ilegood@users.noreply.github.com>
2026-04-08 20:57:20 +0900

    update friendList

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -5,16 +5,15 @@ import Sidebar from "./Sidebar";
 
 const Layout = () => {
   return (
-    <div className="page-shell">
-      <main className="page-content">
-        <Header />
-        {/* Outlet 자리에 현재 경로에 element 페이지 컴포넌트가 들어감 */}
-        <div className="page-wrap">
-          <Sidebar />
+    <div className="page">
+      <Header />
+      <div className="page-wrap">
+        <Sidebar />
+        <main>
           <Outlet />
-          <FriendsList />
-        </div>
-      </main>
+        </main>
+        <FriendsList />
+      </div>
     </div>
   );
 };
--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,41 +1,66 @@
-// import { Link } from "react-router-dom";
+import { useState } from "react";
 import styled from "styled-components";
 
 const FriendsStyles = styled.div`
   border: 2px solid #eee;
   background-color: var(--color-bg);
-  width: 300px;
-  height: 97vh;
+  width: ${({ $isOpen }) => ($isOpen ? "300px" : "70px")};
   padding: 30px;
+  transition: 0.2s ease;
+
+  position: sticky;
+  right: 0;
+  top: 0;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  input {
+    margin: 10px 0 20px;
+    padding: 10px 20px;
+    width: 100%;
+    border-radius: 50px;
+    border: 1px solid #ccc;
+  }
+
+  span {
+    width: 100%;
+    font-size: 18px;
+    font-weight: 500;
+    border-top: 2px solid #ccc;
+    padding: 20px 0;
+    margin-top: 10px;
+  }
+
   ul {
     list-style: none;
   }
 `;
 
 const FriendsList = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isCardOpen, setIsCardOpen] = useState(false);
+
   return (
-    <>
-      <FriendsStyles>
-        <h3>친구 목록</h3>
-        <form>
+    <FriendsStyles $isOpen={isOpen}>
+      <button onClick={() => setIsOpen((prev) => !prev)}>
+        {isOpen ? "닫기" : "열기"}
+      </button>
+      {isOpen && (
+        <>
+          <h2>친구목록</h2>
           <input type="text" placeholder="친구 검색" />
-        </form>
-        <ul className="online">
-          <p>온라인</p>
-          <li>ZZAP</li>
-          <li>타코야끼</li>
-          <li>존못</li>
-          <li>밥</li>
-        </ul>
-        <ul className="offline">
-          <p>오프라인</p>
-          <li>하늘</li>
-          <li>깍두기</li>
-          <li>yuncee</li>
-          <li>기즈여</li>
-        </ul>
-      </FriendsStyles>
-    </>
+
+          <span>온라인</span>
+          <ul></ul>
+
+          <span>오프라인</span>
+          <ul></ul>
+        </>
+      )}
+    </FriendsStyles>
   );
 };
 
