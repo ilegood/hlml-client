01f90aa31e6e1d6744194a826cd4b7c06c17a0f6
Hanuel0119 <kej050119@gmail.com>
2026-08-03 16:46:39 +0900

    Update CategorySelector.js

--- src/hooks/CategorySelector.js
+++ src/hooks/CategorySelector.js
@@ -42,7 +42,14 @@ export default function CategorySelector({
             ) : (
               <>
                 {category}
-                <svg width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3">
+                <svg
+                  width="10"
+                  height="10"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="3"
+                >
                   <path d="M6 9l6 6 6-6" />
                 </svg>
               </>
